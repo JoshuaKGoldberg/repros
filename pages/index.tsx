8d051c94abe8b776209b8909eb47bcc8ff3c229a
Josh Goldberg <me@joshuakgoldberg.com>
2021-11-28 02:14:35 -0500

    Initial commit :sparkles:

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import { Html } from "next/document";
+
+/** Add your relevant code here for the issue to reproduce */
+export default function Home() {
+  return (
+    <Html>
+      <main>
+        <h1>Hello</h1>
+        <p>world</p>
+      </main>
+    </Html>
+  );
+}

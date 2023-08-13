9aa024ce390c7f1c277bf122f94e4fd373d3f3d1
Josh Goldberg <git@joshuakgoldberg.com>
2023-08-12 21:34:19 -0400

    Stage 2: Working caught error from OP

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,3 @@
-import "cypress-iframe";
-
 const GENERIC_TIMEOUT = 1_000;
 
 describe("template spec", () => {
@@ -7,31 +5,43 @@ describe("template spec", () => {
     let iframeWindow;
     let spyErrorIframe;
 
-    cy.visit("http://127.0.0.1:1337");
+    cy.visit("127.0.0.1:1337");
 
-    cy.enter().then((getBody) => {
-      getBody()
-        .its("0.ownerDocument.defaultView")
-        .then((win) => {
-          iframeWindow = win;
-
-          function errorHandler(...args) {
-            console.log("~~~ iframe"); // log appears in console
-            return iframeWindow.console.error(...args);
-          }
-
-          spyErrorIframe = cy
-            .spy(iframeWindow.console, "error", errorHandler)
-            .as("spyErrorIframe");
-        });
+    cy.get("iframe").then(($iframe) => {
+      iframeWindow = $iframe[0].contentWindow;
+      function errorHandler(...args) {
+        console.log("~~~ iframe");
+        return iframeWindow.console.error(...args);
+      }
+      spyErrorIframe = cy.spy(errorHandler);
+      iframeWindow.addEventListener("error", spyErrorIframe);
+      iframeWindow.onerror = spyErrorIframe;
+      cy.spy(iframeWindow.console, "log");
+      cy.spy(iframeWindow.console, "error");
+      cy.spy(iframeWindow, "alert");
     });
 
-    // Using alias should means you don't have to wait
-    cy.get("@spyErrorIframe").should("have.been.calledOnceWith", "Catch me"); // passes
+    let mainWindow;
+    let spyErrorWindow;
 
-    cy.wait(1000).then(() => {
-      // one call logged
-      console.log("~~~ iframe error event", spyErrorIframe.getCalls());
+    cy.window().then((win) => {
+      mainWindow = win;
+      function errorHandler(...args) {
+        console.log("~~~ window");
+        return iframeWindow.console.error(...args);
+      }
+      spyErrorWindow = cy.spy(errorHandler);
+      mainWindow.addEventListener("error", spyErrorWindow);
+      mainWindow.onerror = spyErrorWindow;
+      cy.spy(win, "alert");
+      cy.spy(win.console, "log");
+      cy.spy(win.console, "error");
+    });
+
+    cy.wait(GENERIC_TIMEOUT + 1000).then(() => {
+      const [call] = iframeWindow.console.error.getCalls();
+      expect(call).not.to.equal(undefined);
+      expect(call.args).to.deep.equal(["Catch me"]);
     });
   });
 });
